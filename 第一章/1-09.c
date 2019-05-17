63a37373832005255aea586345af6ded8322a59e
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-05-17 17:09:27 +0800

    計算在輸入EOF之前有多少字元 - ch1.5 字符輸出與輸入
    計算在輸入EOF之前有多少字元 - ch1.5 字符輸出與輸入

---
+++ 第一章/1-09.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	long nc;
+	
+	nc = 0;
+	while(getchar() != EOF)
+		++nc;
+	printf("%ld characters\n", nc);
+    /*
+    註:
+      在Linux上輸出EOF字符的方式: ctrl + D
+      在Windows上輸出EOF字符的方式: ctrl + Z
+    */	
+	return 0;
+} 
