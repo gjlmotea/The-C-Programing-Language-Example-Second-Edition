f59e75eb269496709e8da7740d19517c5634beb2
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-05-21 18:00:52 +0800

    將輸入字符印出(getchar、putchar) - ch1.5 字符輸出與輸入
    將輸入字符印出(getchar、putchar) - ch1.5 字符輸出與輸入

--- ch01/1-07.c
+++ ch01/1-07.c
@@ -8,10 +8,11 @@ int main() {
 		putchar(c);
 		c = getchar();
 	}
-	    /*
-	    註:
-	      在Linux上輸出EOF字符的方式: ctrl + D
-	      在Windows上輸出EOF字符的方式: ctrl + Z
-	    */	
+	/*
+	註:
+	  在Linux上輸出EOF字符的方式: ctrl + D
+	  在Windows上輸出EOF字符的方式: ctrl + Z
+	*/	
+	//ref: http://bbs.chinaunix.net/thread-981231-1-1.html
 	return 0;
 } 
